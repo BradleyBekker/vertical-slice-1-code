8fe7106d44cadbb910e4fc267b96bb3e66119335
DESKTOP-1STIJ1S\bradl <thekid72gaming@gmail.com>
2018-12-21 08:51:05 +0100

    mijn code voor vertical slice 1

---
+++ navmesh/BBNavigator.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BBNavigator : MonoBehaviour {
+
+    public Camera camera;
+    public NavMeshAgent agent;
+    Vector3 safepos;
+    private bool onOverLap;
+    // Update is called once per frame
+    void Update () {
+
+
+
+
+
+
+        Ray edge = new Ray(transform.position,new Vector3(0,-1,0));
+        
+        RaycastHit check;
+
+        if (!Physics.Raycast(edge, out check,3))
+        {
+            agent.SetDestination(safepos);
+
+
+
+        }
+        else
+        {
+            print("safepos");
+            safepos = transform.position;
+
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            
+            RaycastHit hit;
+           
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.name == "Overlap platform")
+                {
+                    agent.SetDestination(new Vector3(5.163997f, -0.5f, 3.049447f));
+                    print("hit");
+                    
+                }
+
+               
+
+
+                if (hit.collider.name != "Midden_hendel_Toren"&& hit.collider.name != "Overlap platform")
+                agent.SetDestination(hit.point);
+                print(hit.collider.name);
+                if (onOverLap && hit.collider.name != "Overlap platform" && hit.collider.name != "Platform 4" && hit.collider.name != "Midden_hendel_Toren")
+                {
+                    print("warped");
+                    agent.Warp(new Vector3(6.3f, -0.455f, 3.039447f));
+                    onOverLap = false;
+                   
+                }
+
+            }
+            else print("no hit");
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "teleport_panel")
+        agent.Warp(new Vector3(11.634f, 4.7f, -3.0f));
+
+        if (other.name == "Overlap platform")
+        {
+            onOverLap = true;
+            print(onOverLap);
+        }
+       
+    }
+
+
+
+
+}
---
+++ navmesh/BBPlayerMove.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    
+public class BBPlayerMove : MonoBehaviour {
+    //float speed = 4;
+    bool canGoRight=false;
+    bool canGoLeft=false;
+    bool canGoForward=false;
+    bool canGoBack=false;
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Ray rightRay = new Ray(transform.position, new Vector3(1, -1, 0));
+        Ray leftRay = new Ray(transform.position, new Vector3(-1 , -1, 0));
+        Ray forwardRay = new Ray(transform.position, new Vector3(0, -1, 1));
+        Ray backRay = new Ray(transform.position, new Vector3(0,-1,-1));
+
+        RaycastHit rightHit;
+        RaycastHit leftHit;
+        RaycastHit forwardHit;
+        RaycastHit backHit;
+        if (Physics.Raycast(rightRay, out rightHit, 3))
+        {
+            canGoRight = true;
+        }canGoRight = false;
+        if (Physics.Raycast(leftRay, out leftHit, 3))
+        {
+            canGoLeft = true;
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * leftHit.distance, Color.yellow);
+        }
+        else canGoLeft = false;
+
+        if (Physics.Raycast(forwardRay, out forwardHit, 3))
+        {
+            canGoForward = true;
+        }
+        else canGoForward = false;
+
+        if (Physics.Raycast(backRay, out backHit, 3))
+        {
+            canGoBack = true;
+        }
+        else canGoBack = false;
+
+        Movement();
+	}
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if(canGoLeft)
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (canGoRight) 
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (canGoBack)
+            transform.Translate(Vector3.back * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (canGoForward)
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+    }
+}
